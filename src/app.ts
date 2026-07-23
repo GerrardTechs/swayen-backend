1a295def8326eed840c7e4df4d87779dbbdeb1c6
GerrardTech <FarrelGerrard17@gmail.com>
2026-07-23 21:33:02 +0700

    tes cors

--- src/app.ts
+++ src/app.ts
@@ -12,10 +12,7 @@ export function createApp(): Application {
   // --- Security & core middleware ---
   app.use(helmet());
   app.use(
-    cors({
-      origin: env.CORS_ORIGIN,
-      credentials: true,
-    })
+    cors()
   );
   app.use(express.json({ limit: "1mb" }));
   app.use(express.urlencoded({ extended: true }));
